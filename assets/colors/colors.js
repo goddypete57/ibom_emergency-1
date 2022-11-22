7b4e69cf0bde4c3fbca657ab78b062fd60cae249
imeofon <ezraimeofon@gmail.com>
2022-11-22 09:54:27 +0100

    colors and fonts

--- assets/colors/colors.js
+++ assets/colors/colors.js
@@ -4,6 +4,12 @@ inactiveColor: '#837670',
 color2: '#FCE8DC',
 red1: '#FF6244',
 primaryColor: '#F87619',
+inacticeGradient1: '#F2C8AD',
+inactiveGradient2: '#7E5D5D',
+linearGradientRed1: '#FF0202',
+linearGradientRed2: '#E41717',
+linearGradientOrange1: '#FC9B5C',
+linearGradientOrange2: '#FF1818',
 };
 
 export default colors;
