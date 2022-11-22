a438b23a266538f691a512b957c3516bb2b8dc28
Vcky4 <okon.victor.u@gmail.com>
2022-11-22 16:14:08 +0100

    setup nav

--- src/navigation/stacks/MainStack.js
+++ src/navigation/stacks/MainStack.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { View, Text, StyleSheet, Image, } from 'react-native';
 
 import colors from '../../../assets/colors/colors';
 import authRoute from '../route/authRoute';
@@ -11,10 +12,27 @@ const Stack = createNativeStackNavigator();
 const NavStack = createNativeStackNavigator();
 
 
-export default function MainStack() {
+const Nav = ({ navigation }) => {
     return (
-        <NavStack.Navigator>
-            <NavStack.Screen name={mainRoute.sos} component={Sos} />
-        </NavStack.Navigator>
+        <>
+            <NavStack.Navigator>
+                <NavStack.Screen name={mainRoute.sos} component={Sos} options={{ headerShown: false }} />
+
+            </NavStack.Navigator>
+            <View style={{
+                backgroundColor: colors.linearGradientOrange1,
+                height: 60,
+            }}>
+
+            </View>
+        </>
+    );
+}
+
+export default MainStack = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name='Nav' component={Nav} options={{ headerShown: false }} />
+        </Stack.Navigator>
     );
 }
