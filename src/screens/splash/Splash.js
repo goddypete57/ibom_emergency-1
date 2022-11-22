1c0aca3b60c528ff87bc865ca3aa0018fe270c54
Vcky4 <okon.victor.u@gmail.com>
2022-11-22 15:32:21 +0100

    splash screen

---
+++ src/screens/splash/Splash.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { View, Text, StyleSheet, Image, } from "react-native";
+
+
+const Splash = () => {
+    return (
+        <View style={styles.container}>
+            {/* <Image style={styles.logo} source={require("../assets/logo.png")} /> */}
+            <Text style={styles.text}>Loading...</Text>
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#000",
+    },
+    logo: {
+        width: 200,
+        height: 200,
+        borderRadius: 100,
+    },
+    text: {
+        color: "#fff",
+        fontSize: 20,
+        fontWeight: "bold",
+        marginTop: 20,
+    },
+});
+
