99d6d2d0c97d3e81d38a1ecedc553b4d60b5dc20
Vcky4 <okon.victor.u@gmail.com>
2022-11-22 15:42:27 +0100

    set app .js

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 import React, { useEffect, useContext, useState } from "react";
-import { StatusBar, Platform } from 'react-native'
+import {StatusBar, Platform } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
-// import Toast from 'react-native-toast-message';
+import Toast from 'react-native-toast-message';
 
 
-import Splash from "./src/screens/onboarding/Splash";
+import Splash from "./src/screens/splash/Splash";
 import { AuthContext, AuthContextProvider } from "./context/AuthContext.js";
 import AuthStack from "./src/navigation/stacks/AuthStack";
 import MainStack from "./src/navigation/stacks/MainStack";
@@ -23,26 +22,11 @@ const RootNavigator = () => {
           token ? <MainStack /> :
             <AuthStack />}
     </NavigationContainer>
-=======
-import React, { useState, useEffect, useContext } from "react";
-import { View, Text, StyleSheet, Image, StatusBar, Platform } from "react-native";
-import MainStacks from "./src/navigation/stacks/GneralStack"
-import { NavigationContainer } from '@react-navigation/native';
-import { AuthContext, AuthContextProvider } from "./context/AuthContext.js";
-import SOSStack from "./src/navigation/stacks/SOSStack";
-const RootNavigator = () => {  
-  return (
-    <NavigationContainer>
- 
-    </NavigationContainer>
-
->>>>>>> 47b1063d9da8b49b0438f6a425285b6ba7d93ea7
   )
 }
 export default function App() {
   if (Platform.OS == 'ios') {
     StatusBar.setBarStyle('light-content', true);	//<<--- add this
-<<<<<<< HEAD
   }
   return (
     <>
@@ -51,16 +35,6 @@ export default function App() {
         <RootNavigator />
       </AuthContextProvider>
       <Toast />
-=======
-}
-  return (
-    <>
-     <AuthContextProvider>
-     <StatusBar backgroundColor={"#000"} />
-      <RootNavigator />
-    </AuthContextProvider>
-
->>>>>>> 47b1063d9da8b49b0438f6a425285b6ba7d93ea7
     </>
   );
 }
