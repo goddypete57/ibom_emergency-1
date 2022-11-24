c8bae102d58d944f488ca26b790ec74d96ed1941
imeofon <ezraimeofon@gmail.com>
2022-11-24 16:31:50 +0100

    colors and fonts and settings screen

--- src/screens/setting/Setting.js
+++ src/screens/setting/Setting.js
@@ -1,25 +1,22 @@
-import React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import React from 'react';
+import { View, Text, StyleSheet, Image, SafeAreaView } from 'react-native';
 import colors from '../../../assets/colors/colors';
 
-
-export default Setting = () => {
+export default Sos = () => {
     return (
         <View style={styles.container}>
 
-            <SafeAreaView>
-                <View style={styles.headerWrapper}>
-                    <Image
-                        source={require('../../../assets/images/Back.png')}
-                        style={styles.backIcon}
-                    />
-                    <Text style={styles.headerText}>Settings</Text>
-                </View>
-            </SafeAreaView>
+            <View style={{
+                height: 70,
+                backgroundColor: colors.appTopBar,
+                flexDirection: 'row',
+            }}>
+
+            </View>
 
             <View style={styles.profileWrapper}>
                 <Image
-                    source={require('../../../assets/images/ProfileImage.png')}
+                    source={require('../../../assets/images/profile-image.png')}
                     style={styles.profileImage}
                 />
                 <View style={styles.profileTextWrapper}>
@@ -27,10 +24,45 @@ export default Setting = () => {
                     <Text style={styles.profileText}>Profile</Text>
                 </View>
                 <Image
-                    source={require('../../../assets/images/Forward.png')}
+                    source={require('../../../assets/images/foward.png')}
                     style={styles.arrowWrapper}
                 />
             </View>
+
+            <View style={styles.curveWrapper}>
+                <View style={styles.accountWrapper}>
+                    <Image
+                        source={require('../../../assets/images/account.png')}
+                        style={styles.accountImage}
+                    />
+                    <Text style={styles.accountName}>Account Settings</Text>
+
+                    <Image
+                        source={require('../../../assets/images/foward.png')}
+                        style={styles.accountArrowWrapper}
+                    />
+                </View>
+                <View style={styles.privacyWrapper}>
+                <Image
+                        source={require('../../../assets/images/privacy.png')}
+                        style={styles.privacyImage}
+                    />
+                    <Text style={styles.privacyName}>Privacy Policy</Text>
+
+                    <Image
+                        source={require('../../../assets/images/foward.png')}
+                        style={styles.privacyArrowWrapper}
+                    />
+                </View>
+                <View style={styles.logoutWrapper}>
+                <Image
+                        source={require('../../../assets/images/logout.png')}
+                        style={styles.logoutImage}
+                    />
+                    <Text style={styles.logoutName}>Logout</Text>
+                </View>
+            </View>
+
         </View>
     );
 };
@@ -39,13 +71,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: colors.white,
-    },
-    headerWrapper: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        paddingHorizontal: '16',
-        paddingTop: '20',
-        backgroundColor: colors.appTopBar,
+        flexDirection: 'column'
     },
     backIcon: {
         width: 24,
@@ -71,6 +97,7 @@ const styles = StyleSheet.create({
     profileTextWrapper: {
         flexDirection: 'column',
         paddingHorizontal: 12,
+        marginTop: 5,
     },
     profileName: {
         fontFamily: 'Outfit-Medium',
@@ -85,6 +112,73 @@ const styles = StyleSheet.create({
     arrowWrapper: {
         width: 24,
         height: 24,
+        marginTop: 5,
         marginStart: 112,
+    },
+    curveWrapper: {
+        marginTop: 25,
+        flexDirection: 'column',
+        marginStart: 6,
+        marginEnd: 6,
+        height: 555,
+        backgroundColor: colors.white,
+        borderTopStartRadius: 20,
+        borderTopEndRadius: 20,
+        shadowColor: colors.linearGradientOrange1,
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.5,
+        shadowRadius: 15,
+        elevation: 5,
+    },
+    accountWrapper: {
+        flexDirection: 'row',
+        marginStart: 10,
+        marginTop: 41,
+    },
+    accountName: {
+        fontFamily: 'Outfit-Regular',
+        fontSize: 16,
+        marginStart: 19,
+        marginTop: 5,
+        color: colors.textColor1,
+    },
+    accountArrowWrapper: {
+        alignContent: 'flex-end',
+        marginStart: 140,
+        marginTop: 5,
+        marginEnd: 32,
+    },
+    privacyWrapper: {
+        flexDirection: 'row',
+        marginStart: 10,
+        marginTop: 30,
+    },
+    privacyName: {
+        fontFamily: 'Outfit-Regular',
+        fontSize: 16,
+        marginStart: 19,
+        marginTop: 5,
+        color: colors.textColor1,
+    },
+    privacyArrowWrapper: {
+        alignContent: 'flex-end',
+        marginStart: 164,
+        marginTop: 5,
+        marginEnd: 32,
+    },
+    logoutWrapper: {
+        flexDirection: 'row',
+        marginStart: 10,
+        marginTop: 34,
+    },
+    logoutName: {
+        fontFamily: 'Outfit-Regular',
+        fontSize: 16,
+        marginTop: 5,
+        marginStart: 19,
+        color: colors.textColor1,
     },
 });
