46c3db2a55df317ac2e9d7e21c425ba9489d28d1
imeofon <ezraimeofon@gmail.com>
2022-11-24 10:07:52 +0100

    colors and fonts and settings screen

--- src/screens/setting/Setting.js
+++ src/screens/setting/Setting.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { View, Text, StyleSheet, Image, } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import colors from '../../../assets/colors/colors';
+import BackArrow from '../../../assets/icons/BackArrow.svg';
 
 
 export default Sos = () => {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Loading...</Text>
+            
+            {/* Header */}
+            <SafeAreaView>
+                <View style={styles.headerWrapper}>
+                    <BackArrow style={styles.backIcon}/>
+                    <Text style={styles.headerText}>Settings</Text>
+                </View>
+            </SafeAreaView>
         </View>
     );
 }
@@ -14,14 +23,18 @@ export default Sos = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
         backgroundColor: colors.white,
     },
-    text: {
-        color: colors.white,
-        fontSize: 20,
-        fontWeight: "bold",
-        marginTop: 20,
+    headerWrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal: '16',
+        paddingTop: '20',
     },
+    backIcon: {
+        width: 24,
+        height: 21,
+    },
+    headerText: {},
+    
 });
