76245014b6a8c0c7d4242a03aaca463fc1cd8fc4
Vcky4 <okon.victor.u@gmail.com>
2022-12-01 10:54:45 +0100

    On main: Merge branch 'main' of https://github.com/blockmay-dev/ibom_emergency

--- src/screens/auth/OnboardScreen.js
+++ src/screens/auth/OnboardScreen.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet,Image} from 'react-native';
 import colors from '../../../assets/colors/colors';
+import GradientText from '../../Component/GradientText';
 
+import Button from '../../Component/Button';
+import { color } from 'react-native-reanimated';
 export default OnboardingScreen = () => {
   return (
     <View style={styles.container}>
       <View style={styles.headerWrapper}>
-        <Text style={styles.headerText}>Welcome to DSP!</Text>
-        <Text>Our mission is to provide help to endangered persons</Text>
+      <Image source={require('../../../assets/images/rename.png')} />
+        <Text style={styles.subtext} >Our mission is to provide help to{'\n'} endangered persons</Text>
+      </View>
+      <View style={styles.imageWrapper}>
+<View style={styles.ImageView}>
+<Image source={require('../../../assets/images/policeImage.png')} />
+</View>
+      </View>
+      <View>
+<Button 
+title={"Set up your account"}
+textColor={colors.white}
+
+buttonStyle={styles.Button}
+
+/>
+<Button 
+title={"Login"}
+textColor={colors.textColor1}
+buttonStyle={styles.Button2}
+color={colors.gray2}
+/>
       </View>
     </View>
   );
@@ -19,10 +42,46 @@ const styles = StyleSheet.create({
     backgroundColor: colors.white,
   },
   headerWrapper:{
-  
+  marginStart:24,
+  marginTop:74
   },
 headerText:{
-    fontFamily:"Oufit-Medium",
-    fontSize:32,
+  color: colors.deepGreen,
+  fontSize: 32,
+  fontFamily: 'Outfit-Medium',
 },
+subtext:{
+  color: colors.lightGreen,
+  fontSize: 18,
+  fontFamily: 'Outfit-Regular',
+  marginTop:5
+},
+
+imageWrapper:{
+  width:312,
+  height:277,
+  backgroundColor:colors.gray,
+alignSelf:'center',
+marginTop:43,
+borderRadius:5
+
+},
+ImageView:{
+alignItems:'center',
+justifyContent:'center',
+marginVertical:26
+},
+Button:{
+  width:312,
+  height:50,
+  alignSelf:'center',
+  marginTop:77,
+},
+Button2:{
+  width:312,
+  height:50,
+  alignSelf:'center',
+  marginTop:20,
+
+}
 });
