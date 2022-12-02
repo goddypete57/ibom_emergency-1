6e522c108b950fbcbefdb98ea5b1f1ced5b1de21
imeofon <ezraimeofon@gmail.com>
2022-12-02 13:57:21 +0100

    colors and fonts and police screen

--- src/screens/police/ReportCrime.js
+++ src/screens/police/ReportCrime.js
@@ -29,10 +29,38 @@ export default Sos = () => {
             <View style={styles.dashedWrapper}>
                 <Image
                     source={require('../../../assets/images/back_white.png')}
-                    style={styles.headerLeft}
+                    style={styles.cameraImage}
                 />
-                <Text>Upload evidence (Photo/Video)</Text>
+                <Text style={styles.evidenceText}>Upload evidence (Photo/Video)</Text>
+                <TouchableOpacity onPress={() => { }}>
+                    <View style={styles.uploadBtn}>
+                        <Text style={styles.uploadText}>Upload</Text>
+                    </View>
+                </TouchableOpacity>
             </View>
+            <View style={styles.selectCrimeWrapper}>
+                <Text style={styles.selectCrimeText}>Select Crime</Text>
+                <Image
+                    source={require('../../../assets/images/forward.png')}
+                    style={styles.leftImage}
+                />
+            </View>
+            <View style={styles.descriptionWrapper}>
+                <Text style={styles.descriptionText}>Add description(Optional) Crime</Text>
+            </View>
+            <View style={styles.locationWrapper}>
+                <Text style={styles.locationText}>Add Location(Required)</Text>
+                <Image
+                    source={require('../../../assets/images/forward.png')}
+                    style={styles.locationImage}
+                />
+            </View>
+            <TouchableOpacity onPress={() => { }}>
+                <View style={styles.submitBtn}>
+                    <Text style={styles.submitText}>Submit</Text>
+                </View>
+            </TouchableOpacity>
+
 
         </View>
     );
@@ -59,6 +87,130 @@ const styles = StyleSheet.create({
         fontFamily: 'Outfit-Medium',
         fontSize: 20,
         marginStart: 33,
+        color: colors.white,
+    },
+    titleText: {
+        fontFamily: 'Outfit-Regular',
+        fontSize: 16,
+        marginStart: 15,
+        marginEnd: 15,
+        marginTop: 26,
+        color: colors.inactiveColor,
+    },
+    dashedWrapper: {
+        borderStyle: 'dashed',
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: colors.inactiveColor,
+        marginTop: 41,
+        marginStart: 17,
+        marginEnd: 17,
+        width: '100%',
+        height: 194,
+        justifyContent: 'center',
+    },
+    cameraImage: {
+        width: 50,
+        height: 50,
+        marginTop: 34,
+    },
+    evidenceText: {
+        fontFamily: 'Outfit-Regular',
+        fontSize: 16,
+        marginTop: 20,
+        color: colors.inactiveColor,
+    },
+    uploadBtn: {
+        width: 110,
+        height: 40,
+        marginTop: 8,
+        backgroundColor: colors.inactiveColor,
+        borderRadius: 15,
+    },
+    uploadText: {
+        fontFamily: 'Outfit-Medium',
+        fontSize: 16,
+        color: colors.white,
+    },
+    selectCrimeWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        borderRadius: 10,
+        borderWidth: 1,
+        padding: 10,
+        borderColor: '#D1D1D6',
+        width: '100%',
+        height: 50,
+        marginStart: 15,
+        marginEnd: 15,
+        marginTop: 49,
+        backgroundColor: colors.white,
+    },
+    selectCrimeText: {
+        fontFamily: 'Outfit-Regular',
+        fontSize: 16,
+        color: colors.inactiveColor,
+    },
+    leftImage: {
+        width: 24,
+        height: 24,
+    },
+    descriptionWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        borderRadius: 10,
+        borderWidth: 1,
+        padding: 10,
+        borderColor: '#D1D1D6',
+        width: '100%',
+        height: 50,
+        marginStart: 15,
+        marginEnd: 15,
+        marginTop: 31,
+        backgroundColor: colors.white,
+    },
+    descriptionText: {
+        fontFamily: 'Outfit-Regular',
+        fontSize: 16,
+        color: colors.inactiveColor,
+    },
+    locationWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        borderRadius: 10,
+        borderWidth: 1,
+        padding: 10,
+        borderColor: '#D1D1D6',
+        width: '100%',
+        height: 50,
+        marginStart: 15,
+        marginEnd: 15,
+        marginTop: 31,
+        backgroundColor: colors.white,
+    },
+    locationText: {
+        fontFamily: 'Outfit-Regular',
+        fontSize: 16,
+        color: colors.inactiveColor,
+    },
+    locationImage: {
+        width: 24,
+        height: 24,
+    },
+    submitBtn: {
+        width: 171,
+        height: 50,
+        justifyContent: 'center',
+        borderRadius: 10,
+        backgroundColor: colors.orange,
+        alignSelf: 'flex-end',
+        marginTop: 50,
+        marginEnd: 17,
+        marginBottom: 35,
+    },
+    submitText: {
+        fontFamily: 'Outfit-Medium',
+        fontSize: 16,
         color: colors.white,
     },
 });
