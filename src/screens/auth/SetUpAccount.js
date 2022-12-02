fe002e21b601926054359df3cf5995f9a2b0e1d3
Vcky4 <okon.victor.u@gmail.com>
2022-12-02 11:04:19 +0100

    set up accout step 1 - fix

--- src/screens/auth/SetUpAccount.js
+++ src/screens/auth/SetUpAccount.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react';
+import React, { useContext, useState } from 'react';
 import {
   View,
   Text,
@@ -9,86 +9,86 @@ import {
   ScrollView
 } from 'react-native';
 import colors from '../../../assets/colors/colors';
-import {AuthContext} from '../../../context/AuthContext';
 import Button from '../../Component/Button';
 import authRoute from '../../navigation/route/authRoute';
-export default SetUpAccount = ({navigation}) => {
-  const {login} = useContext(AuthContext);
+export default SetUpAccount = ({ navigation }) => {
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
-  let canLogin = email !== '' && password !== '';
+  var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+  const canProceed =
+    lastName.length > 0 && firstName.length > 0
+    && emailReg.test(email) && phoneNumber.length > 0;
   return (
     <ScrollView
-    vertical
-    showsHorizontalScrollIndicator={false}
-    contentContainerStyle={{
+      vertical
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{
         flexGrow: 1,
         justifyContent: 'flex-start',
         width: '100%',
-    }}>
- <View style={styles.container}>
-      <View style={styles.headerWrapper}>
-        <Image
-          source={require('../../../assets/images/SetUpYourAccount.png')}
-        />
-        <Text style={styles.subtext}>Enter a valid email and password</Text>
+      }}>
+      <View style={styles.container}>
+        <View style={styles.headerWrapper}>
+          <Image
+            source={require('../../../assets/images/SetUpYourAccount.png')}
+          />
+          <Text style={styles.subtext}>Enter a valid email and password</Text>
+        </View>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          style={styles.inputWrapper}>
+          <View style={styles.Wrapper}>
+            <Text style={styles.text}>First Name</Text>
+            <TextInput
+              style={styles.email}
+              value={firstName}
+              onChangeText={text => setFirstName(text)}
+              selectionColor={'rgba(42, 83, 76, 0.7)'}
+              placeholderTextColor={colors.inactiveColor}
+            />
+
+            <Text style={styles.text2}>Last Name</Text>
+            <TextInput
+              style={styles.email}
+              value={lastName}
+              onChangeText={text => setLastName(text)}
+              selectionColor={'rgba(42, 83, 76, 0.7)'}
+              placeholderTextColor={colors.inactiveColor}
+            />
+
+            <Text style={styles.text2}>Email</Text>
+            <TextInput
+              style={styles.email}
+              value={email}
+              onChangeText={text => setEmail(text)}
+              selectionColor={'rgba(42, 83, 76, 0.7)'}
+              placeholderTextColor={colors.inactiveColor}
+            />
+
+            <Text style={styles.text2}>Phone Number</Text>
+            <TextInput
+              style={styles.email}
+              value={phoneNumber}
+              onChangeText={text => setPhoneNumber(text)}
+              selectionColor={'rgba(42, 83, 76, 0.7)'}
+              placeholderTextColor={'rgba(42, 83, 76, 0.7)'}
+            />
+          </View>
+          <View style={styles.ButtonWrapper}>
+            <Button
+              title={'Go ahead...'}
+              enabled={canProceed}
+              onPress={() => { navigation.navigate(authRoute.setUpAccount2) }}
+              buttonStyle={styles.Button}
+              textColor={colors.white}
+            />
+          </View>
+        </KeyboardAvoidingView>
       </View>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        style={styles.inputWrapper}>
-        <View style={styles.Wrapper}>
-          <Text style={styles.text}>First Name</Text>
-          <TextInput
-            style={styles.firstNmae}
-            value={firstName}
-            onChangeText={text => setFirstName(text)}
-            selectionColor={'rgba(42, 83, 76, 0.7)'}
-            placeholderTextColor={colors.inactiveColor}
-          />
-
-          <Text style={styles.text2}>Last Name</Text>
-          <TextInput
-            style={styles.lastname}
-            value={lastName}
-            onChangeText={text => setLastName(text)}
-            selectionColor={'rgba(42, 83, 76, 0.7)'}
-            placeholderTextColor={colors.inactiveColor}
-          />
-
-          <Text style={styles.text2}>Email</Text>
-          <TextInput
-            style={styles.email}
-            value={email}
-            onChangeText={text => setEmail(text)}
-            selectionColor={'rgba(42, 83, 76, 0.7)'}
-            placeholderTextColor={colors.inactiveColor}
-          />
-
-          <Text style={styles.text2}>Phone Number</Text>
-          <TextInput
-            style={styles.phoneNumber}
-            value={phoneNumber}
-            onChangeText={text => setPhoneNumber(text)}
-            selectionColor={'rgba(42, 83, 76, 0.7)'}
-            placeholderTextColor={'rgba(42, 83, 76, 0.7)'}
-          />
-        </View>
-        <View style={styles.ButtonWrapper}>
-          <Button
-            title={'Go ahead...'}
-            enabled={true}
-            onPress={()=>{navigation.navigate(authRoute.setUpAccount2)}}
-            buttonStyle={styles.Button}
-            textColor={colors.white}
-          />
-        </View>
-      </KeyboardAvoidingView>
-    </View>
     </ScrollView>
-   
+
   );
 };
 
@@ -107,16 +107,6 @@ const styles = StyleSheet.create({
     fontFamily: 'Outfit-Regular',
     marginTop: 5,
   },
-  firstNmae: {
-    paddingVertical: 14,
-    backgroundColor: 'rgba(42, 83, 76, 0.1)',
-    borderRadius: 5,
-    width: '100%',
-    marginTop: 5,
-    fontSize: 18,
-    color: 'rgba(42, 83, 76, 0.7)',
-    fontFamily: 'Outfit-Regular',
-  },
   text: {
     color: 'rgba(42, 83, 77, 0.8)',
     fontSize: 16,
@@ -128,38 +118,20 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontFamily: 'Outfit-Regular',
   },
-  lastname: {
-    paddingVertical: 14,
-    backgroundColor: 'rgba(42, 83, 76, 0.1)',
-    borderRadius: 5,
-    width: '100%',
-    marginTop: 5,
-    fontSize: 18,
-    color:'rgba(42, 83, 76, 0.7)',
-    fontFamily: 'Outfit-Regular',
-  },
   email: {
-    paddingVertical: 14,
+    paddingVertical: 12,
     backgroundColor: 'rgba(42, 83, 76, 0.1)',
     borderRadius: 5,
     width: '100%',
     marginTop: 5,
     fontSize: 18,
-    color:'rgba(42, 83, 76, 0.7)',
+    color: colors.textGreen,
     fontFamily: 'Outfit-Regular',
-  },
-  phoneNumber: {
-    paddingVertical: 14,
-    backgroundColor: 'rgba(42, 83, 76, 0.1)',
-    borderRadius: 5,
-    width: '100%',
-    marginTop: 5,
-    fontSize: 18,
-    color:'rgba(42, 83, 76, 0.7)',
-    fontFamily: 'Outfit-Regular',
+    paddingHorizontal: 12,
   },
 
-  Wrapper: {marginStart: 24, marginEnd: 24, marginTop: 24},
+
+  Wrapper: { marginStart: 24, marginEnd: 24, marginTop: 24 },
 
   Button: {
     width: '100%',
@@ -167,5 +139,5 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     marginTop: 63,
   },
-  ButtonWrapper:{marginStart: 24, marginEnd: 24, marginTop: 24}
+  ButtonWrapper: { marginStart: 24, marginEnd: 24, marginTop: 24 }
 });
