8b3bb461d2bcfc42ba0460b8f190ed8519182bf1
Vcky4 <okon.victor.u@gmail.com>
2022-12-02 15:24:47 +0100

    input field

---
+++ src/Component/InputField.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, TextInput, Text } from "react-native";
+
+import colors from "../assets/colors/colors";
+
+
+export default function InputField({ leftComponet, containerStyle, label, ...rest }) {
+    return (
+        <View style={[{
+            borderColor: colors.black,
+            borderRadius: 8,
+            borderWidth: 1,
+            paddingHorizontal: 10,
+            marginEnd: 10,
+            height: 50,
+        }, containerStyle]}>
+            <Text style={{
+                color: colors.black,
+                fontSize: 10,
+                fontFamily: 'Lato-Regular',
+                backgroundColor: colors.white,
+                transform: [{ translateY: -8 }],
+                paddingHorizontal: 9,
+                textAlign: 'center',
+                position: 'absolute',
+                marginStart: 10,
+            }}>{label}</Text>
+            <View style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+            }}>
+                <TextInput
+                    {...rest}
+                />
+                {leftComponet && leftComponet}
+            </View>
+
+        </View>
+
+    );
+}
