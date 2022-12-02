fef71b2deb417a68d2ad07eda372b013d915dd0b
Clarksworld <emmaclarkworld@gmail.com>
2022-12-02 14:40:51 +0100

    Added map and location getter

--- src/screens/police/police.js
+++ src/screens/police/police.js
@@ -1,13 +1,30 @@
-import React from "react";
-import { View, Text, StyleSheet, Image, } from "react-native";
-
+import React, {useState, useEffect}  from "react";
+import { View, Text, StyleSheet, Image, Dimensions, } from "react-native";
 import colors from '../../../assets/colors/colors';
+import MapView, {PROVIDER_GOOGLE, PROVIDER_DEFAULT} from 'react-native-maps';
 
 
-export default Sos = () => {
+
+export default Police = () => {
+
+
+    const defaultProvider =
+    Platform.OS === 'ios' ? PROVIDER_DEFAULT : PROVIDER_GOOGLE;
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Loading...</Text>
+            {/* <Text style={styles.text}>Loading...</Text> */}
+     <MapView
+      provider={PROVIDER_GOOGLE}
+      style={styles.mapView}
+      initialRegion={{
+        latitude: 4.9057,
+        longitude: 7.8537,
+        latitudeDelta: 0.0025,
+        longitudeDelta: 0.0421,
+      }}>
+  
+      </MapView>
+   
         </View>
     );
 }
@@ -24,5 +41,10 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: "bold",
         marginTop: 20,
+    },
+     mapView: {
+     width: Dimensions.get("window").width,
+     height: Dimensions.get("window").height
+
     },
 });
