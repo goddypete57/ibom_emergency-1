5770fd7e16fd36c9214b9c48d885b9bb3486f1cc
Vcky4 <okon.victor.u@gmail.com>
2022-12-03 22:32:36 +0100

    create patrol result

---
+++ src/screens/map/PatrolResult.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, Image, Dimensions, } from "react-native";
+import colors from '../../../assets/colors/colors';
+import MapView, { PROVIDER_GOOGLE, PROVIDER_DEFAULT } from 'react-native-maps';
+
+
+
+export default PatrolResult = ({ navigation }) => {
+    return (
+        <View style={styles.container}>
+
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
