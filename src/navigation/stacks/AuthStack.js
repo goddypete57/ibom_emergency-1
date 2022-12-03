552b30c9b264fe531c60b0e8b6d0db8f6a2115d2
Vcky4 <okon.victor.u@gmail.com>
2022-12-03 13:28:30 +0100

    add to nav

--- src/navigation/stacks/AuthStack.js
+++ src/navigation/stacks/AuthStack.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import authRouts from '../route/authRoute';
 import Login from '../../../src/screens/auth/Login';
 import OnboardingScreen from '../../../src/screens/auth/OnboardScreen';
 import SetUpAccount from '../../../src/screens/auth/SetUpAccount';
 import SetUpAccount2 from '../../screens/auth/SetUpAccount2';
 import verifyOtp from '../../screens/auth/verifyOtp';
+import Illusion from '../../screens/auth/Illusion';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,31 +14,36 @@ export default AuthStack = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen
+        name={authRouts.illusion}
+        component={Illusion}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
         name={authRouts.verifyOtp}
         component={verifyOtp}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
 
       <Stack.Screen
         name={authRouts.onbordingScreen}
         component={OnboardingScreen}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
       <Stack.Screen
         name={authRouts.setUpAccount}
         component={SetUpAccount}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
       <Stack.Screen
         name={authRouts.setUpAccount2}
         component={SetUpAccount2}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
 
       <Stack.Screen
         name={authRouts.login}
         component={Login}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
     </Stack.Navigator>
   );
