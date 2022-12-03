c932d51751aa8b391c9e5a230e08a85db3bc6a15
Vcky4 <okon.victor.u@gmail.com>
2022-12-03 22:35:24 +0100

    header

--- src/screens/map/PatrolResult.js
+++ src/screens/map/PatrolResult.js
@@ -3,12 +3,40 @@ import { View, Text, StyleSheet, Image, Dimensions, } from "react-native";
 import colors from '../../../assets/colors/colors';
 import MapView, { PROVIDER_GOOGLE, PROVIDER_DEFAULT } from 'react-native-maps';
 
+import colors from '../../../assets/colors/colors';
+import { AuthContext } from '../../../context/AuthContext';
+import endpoints from '../../../assets/EndPoint/Endpoint';
 
 
 export default PatrolResult = ({ navigation }) => {
+    const { user, token } = useContext(AuthContext);
     return (
         <View style={styles.container}>
-
+            <View style={styles.header}>
+                <View style={styles.header}>
+                    <TouchableOpacity>
+                    </TouchableOpacity>
+                    <View style={styles.textandImageWrapper}>
+                        <View style={styles.textWrapper}>
+                            <Text style={styles.welcomeText}>
+                                Welcome <Text style={styles.userNmae}>{user ? user.firstName : ''}</Text>
+                                {'\n  '}
+                                <Text style={styles.profileStatus}>Complete profile</Text>
+                            </Text>
+                        </View>
+                        <Image
+                            source={{
+                                uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_mNmpqHOTakNgIaKR5bxJFfkUtiLdPBXPMw&usqp=CAU',
+                            }}
+                            style={{
+                                width: 30,
+                                height: 30,
+                                borderRadius: 20,
+                            }}
+                        />
+                    </View>
+                </View>
+            </View>
         </View>
     )
 }
@@ -17,5 +45,34 @@ export default PatrolResult = ({ navigation }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 24,
+        marginLeft: 16,
+        marginRight: 19,
+    },
+    textandImageWrapper: {
+        flexDirection: 'row',
+    },
+    textWrapper: {
+        marginEnd: 5,
+    },
+
+    welcomeText: {
+        color: colors.brown,
+        fontSize: 16,
+        fontFamily: 'Outfit-Regular',
+    },
+    userNmae: {
+        color: colors.brown,
+        fontSize: 16,
+        fontFamily: 'Outfit-Medium',
+    },
+    profileStatus: {
+        color: colors.red,
+        fontSize: 16,
+        fontFamily: 'Outfit-Medium',
     },
 });
