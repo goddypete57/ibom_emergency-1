f3e526d31263263c0b40f620d8ab2b8aa8fb396f
godwin <goddypete56@gmail.com>
2022-12-05 11:37:59 +0100

    changes

--- src/screens/auth/Login.js
+++ src/screens/auth/Login.js
@@ -167,7 +167,7 @@ export default Login = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.background,
+    backgroundColor: colors.white,
   },
   header:{
     color: colors.green3,
